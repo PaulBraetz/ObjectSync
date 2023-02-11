24bdd6ca6c7bf3b3549840f28396b98aa4445dfe
Paul Brätz <paul.braetz@rhomicro.com>
2023-02-11 15:59:28 +0100

    removed SynchronizedIdAttribute comment

--- Generator/GeneratedAttributes.cs
+++ Generator/GeneratedAttributes.cs
@@ -210,24 +210,6 @@ namespace RhoMicro.ObjectSync.Attributes
 
 namespace RhoMicro.ObjectSync.Attributes
 {
-	/// <summary>
-	/// <para>
-	/// Denotes the synchronization authority for synchronized fields of this type. 
-	/// The type returned must provide the following methods:
-	/// </para>
-	/// <para>
-	/// <c>TProperty Pull&lt;TProperty&gt;(String typeId, String propertyName, String sourceInstanceId, String instanceId)</c>
-	/// </para>
-	/// <para>
-	/// <c>void Push&lt;TProperty&gt;(String typeId, String propertyName, String sourceInstanceId, String instanceId, TProperty value)</c>
-	/// </para>
-	/// <para>
-	/// <c>void Subscribe&lt;TProperty&gt;(String typeId, String propertyName, String sourceInstanceId, String instanceId, Action&lt;TProperty&gt; callback)</c>
-	/// </para>
-	/// <para>
-	/// <c>void Unsubscribe(String typeId, String propertyName, String sourceInstanceId, String instanceId)</c>
-	/// </para>
-	/// </summary>
     [AttributeUsage(AttributeTargets.Property, Inherited = false)]
 	internal sealed class SourceInstanceIdAttribute : Attribute
 	{
